1360247690ac7ef0e8601ea3d537b14eabb701c5
Brian May <brian@linuxpenguins.xyz>
2020-09-12 12:23:26 +1000

    Add basic guardian stuff

---
+++ lib/penguin_memories/accounts/user.ex
@@ -0,0 +1,53 @@
+defmodule PenguinMemories.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :is_admin, :boolean, default: false
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+    field :password_hash, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def create_changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :is_admin, :password, :password_confirmation])
+    |> validate_required([:username, :password, :password_confirmation])
+    |> validate_length(:password, min: 8)
+    |> validate_confirmation(:password)
+    |> put_password_hash
+    |> put_change(:password, nil)
+    |> put_change(:password_confirmation, nil)
+  end
+
+  @doc false
+  def update_changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :is_admin])
+    |> validate_required([:username])
+  end
+
+  @doc false
+  def password_changeset(user, attrs) do
+    user
+    |> cast(attrs, [:password, :password_confirmation])
+    |> validate_required([:password, :password_confirmation])
+    |> validate_length(:password, min: 8)
+    |> validate_confirmation(:password)
+    |> put_password_hash
+    |> put_change(:password, nil)
+    |> put_change(:password_confirmation, nil)
+  end
+
+  defp put_password_hash(
+         %Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset
+       ) do
+    change(changeset, password_hash: Argon2.hash_pwd_salt(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
---
+++ priv/repo/migrations/20200912015501_create_users.exs
@@ -0,0 +1,14 @@
+defmodule PenguinMemories.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :username, :string
+      add :password_hash, :string
+      add :is_admin, :boolean, default: false, null: false
+
+      timestamps()
+    end
+
+  end
+end
