b7077167f4a015020263300c0ffe1eab76906f31
Brian May <brian@linuxpenguins.xyz>
2021-05-26 08:02:20 +1000

    Use prettier for styling html files

--- mix.exs
+++ mix.exs
@@ -76,7 +76,8 @@ defmodule PenguinMemories.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      prettier: "cmd ./assets/node_modules/.bin/prettier --check . --color"
     ]
   end
 
