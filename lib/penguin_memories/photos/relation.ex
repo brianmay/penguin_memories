74cf2f606460f1aac520f4252ca3c02383eaaca8
Brian May <brian@linuxpenguins.xyz>
2021-06-03 20:23:09 +1000

    Remove legacy commented out code

--- lib/penguin_memories/photos/relation.ex
+++ lib/penguin_memories/photos/relation.ex
@@ -19,28 +19,4 @@ defmodule PenguinMemories.Photos.Relation do
     field :description, :string
     timestamps()
   end
-
-  # @spec edit_changeset(t(), map()) :: Changeset.t()
-  # def edit_changeset(%__MODULE__{} = album, attrs) do
-  #   album
-  #   |> cast(attrs, [
-  #     :name,
-  #     :description
-  #   ])
-  #   |> validate_required([:name])
-  # end
-
-  # @spec update_changeset(t(), MapSet.t(), map()) :: Changeset.t()
-  # def update_changeset(%__MODULE__{} = album, enabled, attrs) do
-  #   allowed_list = [:name, :description]
-  #   allowed = MapSet.new(allowed_list)
-  #   enabled = MapSet.intersection(enabled, allowed)
-  #   enabled_list = MapSet.to_list(enabled)
-  #   required = MapSet.new([:name])
-  #   required_list = MapSet.to_list(MapSet.intersection(enabled, required))
-
-  #   album
-  #   |> cast(attrs, enabled_list)
-  #   |> validate_required(required_list)
-  # end
 end
