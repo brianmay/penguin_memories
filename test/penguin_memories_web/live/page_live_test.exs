ed249b7f867b51bbb8e3a6da974fdca67dd6df0a
Brian May <brian@linuxpenguins.xyz>
2021-06-29 09:57:15 +1000

    Remove default template code

--- test/penguin_memories_web/live/page_live_test.exs
+++ test/penguin_memories_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule PenguinMemoriesWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "Welcome to Penguin Memories!"
+    assert render(page_live) =~ "Welcome to Penguin Memories!"
   end
 end
