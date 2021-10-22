1cc0b3e188a38d61195eea461ef4d218e6804e78
Brian May <brian@linuxpenguins.xyz>
2021-10-22 11:17:54 +1100

    Delete item_class - not required

--- lib/penguin_memories_web/views/layout_view.ex
+++ lib/penguin_memories_web/views/layout_view.ex
@@ -1,12 +1,6 @@
 defmodule PenguinMemoriesWeb.LayoutView do
   use PenguinMemoriesWeb, :view
 
-  def item_class(active, item) do
-    ["nav-item"]
-    |> prepend_if(active == item, "active")
-    |> Enum.join(" ")
-  end
-
   def link_class(active, item) do
     ["nav-link"]
     |> prepend_if(active == item, "active")
