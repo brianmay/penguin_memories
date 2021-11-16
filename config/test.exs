0a5cfed5e7b93dd20ec84a9670d503602daa4353
Brian May <brian@linuxpenguins.xyz>
2021-11-16 11:01:06 +1100

    Update other stuff

--- config/test.exs
+++ config/test.exs
@@ -26,8 +26,7 @@ config :penguin_memories, PenguinMemories.Repo,
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :penguin_memories, PenguinMemoriesWeb.Endpoint,
-  server: false
+config :penguin_memories, PenguinMemoriesWeb.Endpoint, server: false
 
 # Print only warnings and errors during test
 config :logger, level: :warn
