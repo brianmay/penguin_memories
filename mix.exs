d01d3690621d29eede9d913bb8105ad7cdcfb58b
Brian May <brian@linuxpenguins.xyz>
2021-11-28 09:33:59 +1100

    Update Docker deploy

--- mix.exs
+++ mix.exs
@@ -87,7 +87,8 @@ defmodule PenguinMemories.MixProject do
     [
       ignore_warnings: "dialyzer.ignore-warnings",
       plt_core_path: "priv/plts",
-      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+      plt_add_apps: [:ex_unit]
     ]
   end
 end
