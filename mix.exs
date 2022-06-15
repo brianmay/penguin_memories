f8108449a5c7d4c52845bf43159407b5248ed9f5
Brian May <brian@linuxpenguins.xyz>
2022-06-15 13:14:12 +1000

    Remove references to prettier

--- mix.exs
+++ mix.exs
@@ -78,8 +78,7 @@ defmodule PenguinMemories.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      prettier: "cmd ./assets/node_modules/.bin/prettier --check . --color"
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 
