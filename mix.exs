9d6ce975fb99b5853b152587648aece45d093f01
Brian May <brian@linuxpenguins.xyz>
2024-08-04 18:56:07 +1000

    Make migrations verbose during CI

--- mix.exs
+++ mix.exs
@@ -81,7 +81,7 @@ defmodule PenguinMemories.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 
