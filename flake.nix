9f7f7b681e09b483a80d37a67e67fe6a4e980784
Brian May <brian@linuxpenguins.xyz>
2024-09-23 07:44:49 +1000

    fix: reformat flake.nix

--- flake.nix
+++ flake.nix
@@ -2,61 +2,65 @@
   description = "Photo Database";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-24.05"; };
-    flake-utils = { url = "github:numtide/flake-utils"; };
-    devenv = { url = "github:cachix/devenv"; };
+    nixpkgs = {url = "github:NixOS/nixpkgs/nixos-24.05";};
+    flake-utils = {url = "github:numtide/flake-utils";};
+    devenv = {url = "github:cachix/devenv";};
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, devenv }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        inherit (pkgs.lib) optional optionals;
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    devenv,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      inherit (pkgs.lib) optional optionals;
+      pkgs = nixpkgs.legacyPackages.${system};
 
-        elixir = pkgs.beam.packages.erlang.elixir;
-        beamPackages = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang;
+      elixir = pkgs.beam.packages.erlang.elixir;
+      beamPackages = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang;
 
-        src = ./.;
-        version = "0.0.0";
-        pname = "penguin_memories";
+      src = ./.;
+      version = "0.0.0";
+      pname = "penguin_memories";
 
-        mixFodDeps = beamPackages.fetchMixDeps {
-          TOP_SRC = src;
-          pname = "${pname}-mix-deps";
-          inherit src version;
-          hash = "sha256-DVVT2jGdy+aLBH0QqUg5gMyp8TDcSd2ORwWKOIBDdEE=";
-          # hash = pkgs.lib.fakeHash;
-        };
+      mixFodDeps = beamPackages.fetchMixDeps {
+        TOP_SRC = src;
+        pname = "${pname}-mix-deps";
+        inherit src version;
+        hash = "sha256-DVVT2jGdy+aLBH0QqUg5gMyp8TDcSd2ORwWKOIBDdEE=";
+        # hash = pkgs.lib.fakeHash;
+      };
 
-        nodejs = pkgs.nodejs;
-        nodePackages =
-          import assets/default.nix { inherit pkgs system nodejs; };
+      nodejs = pkgs.nodejs;
+      nodePackages =
+        import assets/default.nix {inherit pkgs system nodejs;};
 
-        pkg = beamPackages.mixRelease {
-          TOP_SRC = src;
-          inherit pname version elixir src mixFodDeps;
+      pkg = beamPackages.mixRelease {
+        TOP_SRC = src;
+        inherit pname version elixir src mixFodDeps;
 
-          postBuild = ''
-            ln -sf ${mixFodDeps}/deps deps
-            ln -sf ${nodePackages.nodeDependencies}/lib/node_modules assets/node_modules
-            export PATH="${nodePackages.nodeDependencies}/bin:$PATH"
-            ${nodejs}/bin/npm run deploy --prefix ./assets
+        postBuild = ''
+          ln -sf ${mixFodDeps}/deps deps
+          ln -sf ${nodePackages.nodeDependencies}/lib/node_modules assets/node_modules
+          export PATH="${nodePackages.nodeDependencies}/bin:$PATH"
+          ${nodejs}/bin/npm run deploy --prefix ./assets
 
-            # for external task you need a workaround for the no deps check flag
-            # https://github.com/phoenixframework/phoenix/issues/2690
-            mix do deps.loadpaths --no-deps-check, phx.digest
-            mix phx.digest --no-deps-check
-          '';
-
-        };
-
-        psql = pkgs.writeShellScriptBin "pm_psql" ''
-          exec "${pkgs.postgresql}/bin/psql" "$DATABASE_URL" "$@"
+          # for external task you need a workaround for the no deps check flag
+          # https://github.com/phoenixframework/phoenix/issues/2690
+          mix do deps.loadpaths --no-deps-check, phx.digest
+          mix phx.digest --no-deps-check
         '';
+      };
 
-        devShell = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [{
+      psql = pkgs.writeShellScriptBin "pm_psql" ''
+        exec "${pkgs.postgresql}/bin/psql" "$DATABASE_URL" "$@"
+      '';
+
+      devShell = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          {
             enterShell = ''
               export HTTP_URL="http://localhost:6001"
               export PORT="6001"
@@ -79,7 +83,8 @@
                 imagemagick
                 ffmpeg-headless
                 libraw
-              ] ++ optional stdenv.isLinux inotify-tools
+              ]
+              ++ optional stdenv.isLinux inotify-tools
               ++ optional stdenv.isDarwin terminal-notifier
               ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
                 CoreFoundation
@@ -87,10 +92,10 @@
               ]);
             services.postgres = {
               enable = true;
-              package = pkgs.postgresql_15.withPackages (ps: [ ps.postgis ]);
+              package = pkgs.postgresql_15.withPackages (ps: [ps.postgis]);
               listen_addresses = "127.0.0.1";
               port = 6000;
-              initialDatabases = [{ name = "penguin_memories"; }];
+              initialDatabases = [{name = "penguin_memories";}];
               initialScript = ''
                 \c penguin_memories;
                 CREATE USER penguin_memories with encrypted password 'your_secure_password_here';
@@ -98,16 +103,17 @@
                 ALTER USER penguin_memories WITH SUPERUSER;
               '';
             };
-          }];
-        };
-
-      in {
-        packages = {
-          devenv-up = devShell.config.procfileScript;
-          default = pkg;
-        };
-        inherit devShell;
-      }) // {
-        nixosModules.default = import ./module.nix { inherit self; };
+          }
+        ];
       };
+    in {
+      packages = {
+        devenv-up = devShell.config.procfileScript;
+        default = pkg;
+      };
+      inherit devShell;
+    })
+    // {
+      nixosModules.default = import ./module.nix {inherit self;};
+    };
 }
