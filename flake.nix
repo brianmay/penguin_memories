ff5a1503213cb6027a71095f6815f031b207eca7
brianmay <brianmay@users.noreply.github.com>
2026-08-01 03:44:43 +0000

    [dependabot skip] Automatically fix Nix hashes

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
           TOP_SRC = src;
           pname = "${pname}-mix-deps";
           inherit src version;
-          hash = "sha256-+Sr5rzvJFEz8DgKYT3ZxpYX0PTlJgi44Ytr2mAFBol8=";
+          hash = "sha256-O1Nv9eawdQBuYG4o7mLyOXE7xjYupiMV0ZdO8atfPoU=";
           # hash = pkgs.lib.fakeHash;
         };
 
